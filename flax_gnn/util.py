a51cf6142e67cd316f18f5d1dacd97347f2c2a14
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2024-06-06 11:07:59 -0500

    Edge feature handling in add_self_edges function

--- flax_gnn/util.py
+++ flax_gnn/util.py
@@ -1,24 +1,23 @@
 import jax.numpy as jnp
-from typing import Tuple
+from typing import Optional, Tuple
 import jraph
 import jax
 
 
-def add_self_edges_fn(receivers: jnp.ndarray, senders: jnp.ndarray,
-                      total_num_nodes: int) -> Tuple[jnp.ndarray, jnp.ndarray]:
-  """Adds self edges. Assumes self edges are not in the graph yet."""
-  # TODO: This does not add edge features for self edges, which might cause shape problems.
-  receivers = jnp.concatenate((receivers, jnp.arange(total_num_nodes)), axis=0)
-  senders = jnp.concatenate((senders, jnp.arange(total_num_nodes)), axis=0)
-  return receivers, senders
-
-
 def add_self_edges(graph: jraph.GraphsTuple) -> jraph.GraphsTuple:
   """Add self edges and return a new graph"""
+
   total_num_nodes = jax.tree.leaves(graph.nodes)[0].shape[0]
-  receivers, senders = add_self_edges_fn(
-      graph.receivers, graph.senders, total_num_nodes)
-  return graph._replace(receivers=receivers, senders=senders)
+  senders = jnp.concatenate(
+      (graph.senders, jnp.arange(total_num_nodes)), axis=0)
+  receivers = jnp.concatenate(
+      (graph.receivers, jnp.arange(total_num_nodes)), axis=0)
+  edges = graph.edges
+  if edges is not None:
+    edges = jnp.concatenate(
+        (edges, jnp.zeros((total_num_nodes, *edges.shape[1:]))),
+        axis=0)
+  return graph._replace(senders=senders, receivers=receivers, edges=edges)
 
 
 def mish(x: jnp.ndarray) -> jnp.ndarray:
