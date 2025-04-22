Fix masking in segment softmax

flax_gnn/util.py
@@ -363,7 +363,7 @@ def segment_softmax(logits: jnp.ndarray,
   maxs = segment_max(logits, segment_ids, num_segments, indices_are_sorted,
                      unique_indices)
   logits = logits - maxs[segment_ids]
-  logits = jnp.where(jnp.isinf(logits), 0.0, logits)
+  logits = jnp.where(jnp.isinf(logits), jnp.finfo(logits).min, logits)
   # Then take the exp
   logits = jnp.exp(logits)
   # Then calculate the normalizers